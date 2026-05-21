802b46405592e690b4fa8fdd09c77207217a7ece
Satoureireal <thamtrungnghia8@gmail.com>
2026-05-21 13:22:19 +0700

    versions 0.1.0

---
+++ assets/js/custom.js
@@ -0,0 +1,76 @@
+(function () {
+
+	'use strict'
+
+
+	AOS.init({
+		duration: 800,
+		easing: 'slide',
+		once: true
+	});
+
+	var preloader = function() {
+
+		var loader = document.querySelector('.loader');
+		var overlay = document.getElementById('overlayer');
+
+		function fadeOut(el) {
+			el.style.opacity = 1;
+			(function fade() {
+				if ((el.style.opacity -= .1) < 0) {
+					el.style.display = "none";
+				} else {
+					requestAnimationFrame(fade);
+				}
+			})();
+		};
+
+		setTimeout(function() {
+			fadeOut(loader);
+			fadeOut(overlay);
+		}, 200);
+	};
+	preloader();
+
+
+	var tinyslider = function() {
+
+
+		var el = document.querySelectorAll('.testimonial-slider');
+		
+
+
+		if ( el.length > 0 ) {
+			var tnsSlider = tns({
+				container: '#testimonial-slider',
+				mode: 'carousel',
+				speed: 700,
+				items: 3,
+				gutter: 32,
+				autoplay: true,
+				autoplayTimeout: 3000,
+				autoplayButtonOutput: false,
+				controlsContainer: '#testimonial-nav',
+				responsive: {
+					0: {
+						items: 1,
+						gutter: 16
+					},
+					700: {
+						items: 2,
+						gutter: 24
+					},
+					1000: {
+						items: 3,
+						gutter: 32
+					}
+				}
+			});
+		}
+
+	}
+	tinyslider();
+
+
+
+})()
